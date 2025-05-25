b205a90f244f677a8d9e7692df3f8dfab38ffe6f
sidhulyalkar <sid.soccer.21@gmail.com>
2025-05-24 17:41:22 -0700

    fix(terraform): wire S3 input prefix to Lambda with correct permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,26 @@ resource "aws_lambda_function" "fft_lambda" {
   timeout       = 60
   memory_size   = 512
 }
+
+# Allow S3 to invoke the Lambda function
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fft_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.data_bucket.arn
+}
+
+# Configure S3 event notification to trigger the Lambda on new pngs under "input/"
+resource "aws_s3_bucket_notification" "bucket_notify" {
+  bucket = aws_s3_bucket.data_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.fft_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "input/"
+    filter_suffix       = ".png"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
+}
